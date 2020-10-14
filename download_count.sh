c12144d08f97ea9826b17ed626898532c8b0b462
Jason Stajich <jasonstajich.phd@gmail.com>
2020-10-14 16:34:37 -0700

    Update download_count.sh

--- download_count.sh
+++ download_count.sh
@@ -2,3 +2,5 @@
 # your script goes below here
 # write a step which will download this file  https://ftp.ncbi.nlm.nih.gov/pub/UniVec/UniVec_Core
 # follow that with steps which will count the number of sequences in the file (this is in the FASTA format)
+
+wget xyxz 

ac073b47ec18a2ac21d8ff93364a35ef38ad40e9
Jason Stajich <jasonstajich.phd@gmail.com>
2020-10-17 14:36:43 -0700

    count strands

--- summary_exons.sh
+++ summary_exons.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/bash
 # your code goes next
+
+curl --no-progress-meter 'https://raw.githubusercontent.com/biodataprog/GEN220_data/main/data/rice_random_exons.bed' | awk '{ sum += $3-$2 } END { print sum}'

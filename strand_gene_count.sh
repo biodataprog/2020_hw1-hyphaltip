f93a397981217b4a917ce0eb21d2b9d777bc6bb5
Jason Stajich <jasonstajich.phd@gmail.com>
2020-10-18 09:17:52 -0700

    stratify by feature type

--- strand_gene_count.sh
+++ strand_gene_count.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/bash
 # your code goes after this 
+URL=https://fungidb.org/common/downloads/release-48/ScerevisiaeS288c/gff/data/FungiDB-48_ScerevisiaeS288c.gff
+
+curl --no-progress-meter $URL | grep -v ^# | cut -f3,7  | sort | uniq -c
+curl --no-progress-meter $URL | grep -v ^# | cut -f3,7  | grep gene | sort | uniq -c

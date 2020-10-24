8427cdd2ec1d5086a8f62180902c4efee5975bbc
Jason Stajich <jasonstajich.phd@gmail.com>
2020-10-24 09:26:20 -0700

    Update download_count.sh

--- download_count.sh
+++ download_count.sh
@@ -7,5 +7,6 @@
 curl -O https://ftp.ncbi.nlm.nih.gov/pub/UniVec/UniVec_Core
 grep -c "^>" UniVec_Core
 
-curl --no-progress-meter https://ftp.ncbi.nlm.nih.gov/pub/UniVec/UniVec_Core | grep -c "^>"
+# on some systems you can add --no-progress-meter on othes you can use -s or --silent and it will not print the download messages
+curl --silent https://ftp.ncbi.nlm.nih.gov/pub/UniVec/UniVec_Core | grep -c "^>"
 
